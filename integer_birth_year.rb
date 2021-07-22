509d6a6e45112bf15263b37b4d73f6bb54e13094
syinGit <syin1@chicagobooth.edu>
2021-07-22 01:36:09 +0000

    integer-chapter_final

--- integer_birth_year.rb
+++ integer_birth_year.rb
@@ -7,3 +7,9 @@
 #   "Wow, you were born in 1940. You're old!"
 
 p "How old are you?"
+
+age_input = gets.to_i
+
+birth_year = 2021 - age_input
+
+p "Wow, you were born in #{birth_year}. You're old!"
